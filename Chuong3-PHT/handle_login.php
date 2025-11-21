cb303eb2564f7a062d731c0f9001f4850bf7a68d
tranquanganh141204@gmail.com <tranquanganh141204@gmail.com>
2025-11-21 22:17:42 +0700

    done

---
+++ Chuong3-PHT/handle_login.php
@@ -0,0 +1,32 @@
+<?php 
+// TODO 1: Khởi động session
+session_start();
+
+// TODO 2: Kiểm tra xem người dùng đã gửi form chưa 
+if (isset($_POST['username']) && isset($_POST['password'])) {
+
+    // TODO 3: Lấy dữ liệu từ POST
+    $user = $_POST['username'];
+    $pass = $_POST['password'];
+
+    // TODO 4: Kiểm tra đăng nhập (giả lập)
+    if ($user == 'quanganh' && $pass == '1412') {
+
+        // TODO 5: Lưu username vào SESSION
+        $_SESSION['logged_user'] = $user;
+
+        // TODO 6: Chuyển hướng sang trang chào mừng
+        header('Location: welcome.php');
+        exit;
+    } else {
+        // Sai thông tin → quay lại login.html + báo lỗi
+        header('Location: login.html?error=1');
+        exit;
+    }
+
+} else {
+    // TODO 7: Truy cập trực tiếp file này mà không POST 
+    header('Location: login.html');
+    exit;
+}
+?>
