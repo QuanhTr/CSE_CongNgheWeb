d0d4def10d44a1c853af0d0c904fc4b35d6e63a3
tranquanganh141204@gmail.com <tranquanganh141204@gmail.com>
2025-12-18 09:26:25 +0700

    update

---
+++ baitap/Chuong6-PHT/Chuong7/PageController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    // TODO 8: Thêm phương thức này
+    public function showHomepage()
+    {
+        $viewTitle = 'PHT Chương 7 - Blade Template'; 
+        $pageTitle = 'Chào mừng bạn đến với Blade!'; 
+        $pageDescription = 'Đây là trang chủ được render bằng Blade Template Engine.'; 
+        $tasks = [ 
+            'Cài đặt Laravel', 
+            'Hiểu về Routing & Controller', 
+            'Tạo Layout với Blade', 
+            'Truyền dữ liệu cho View' 
+        ]; 
+ 
+        // TODO 8: Trả về một View 
+        // Gợi ý: Dùng hàm view('ten_view', $data_array) 
+        // 'homepage' tương đương 'homepage.blade.php' 
+                 return view('home', [ 
+            'title' => $viewTitle, 
+            'page_title' => $pageTitle, 
+            'page_description' => $pageDescription, 
+            'tasks' => $tasks 
+        ]); 
+ 
+        // TODO 9: (Cách khác) Dùng hàm compact() cho gọn 
+         return view('homepage', compact('viewTitle', 'pageTitle', 'pageDescription', 'tasks')); 
+    }
+}
